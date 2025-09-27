bb9a66d1327eba73aa0de652c9639998a452b9a8
bathiste <marcellakomiec4291@gmail.com>
2025-09-27 23:43:50 +0100

    Add files via upload

--- ipgrabs.py
+++ ipgrabs.py
@@ -1,12 +1,14 @@
-import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return f"Server saw IP: {request.remote_addr}\n"
+    # try proxy header first
+    ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+    return f"Server saw IP: {ip}\n"
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
